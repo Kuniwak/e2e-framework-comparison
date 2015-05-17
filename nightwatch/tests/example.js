aa49bb03696750b8424ad4db932fd1006bdcbbfc
Kuniwak <yuki.kokubun@mixi.co.jp>
2015-05-17 19:07:08 +0900

    Fix a test bug caused by the search button hiding behind the autocompletion

--- nightwatch/tests/example.js
+++ nightwatch/tests/example.js
@@ -1,11 +1,11 @@
+'use strict';
 module.exports = {
-  'Demo test Google' : function (browser) {
+  'Demo test Google': function (browser) {
     browser
       .url('http://www.google.com')
       .waitForElementVisible('body', 1000)
       .setValue('input[type=text]', 'nightwatch')
-      .waitForElementVisible('button[name=btnG]', 1000)
-      .click('button[name=btnG]')
+      .keys('\uE007')
       .pause(1000)
       .assert.containsText('#main', 'Night Watch')
       .end();
